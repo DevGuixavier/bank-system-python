c4aa935f4233cbd5631ad2a6dc44c876d3e3fbfe
DevGuixavier <dev.guixavier@gmail.com>
2025-02-16 14:15:27 -0300

    users

---
+++ users.py
@@ -0,0 +1,6 @@
+contas = {
+    "user1": {"senha": "1337", "saldo": 0, "extrato": "", "numero_saques": 0},
+    "user2": {"senha": "ab1337", "saldo": 1500, "extrato": "Depósito inicial: R$ 1000.00\n", "numero_saques": 0},
+}
+LIMITE_SAQUES = 5
+LIMITE_VALOR_SAQUE = 1500
